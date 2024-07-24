3e361af6f13fd9166f88fb1b32d35030ed5a48a4
Bree2003 <breeale2003@gmail.com>
2024-07-24 10:29:39 -0400

    connect with the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DATABASE, HOST, PASSWORD, PORT, USERNAME } from 'config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: HOST,
+      port: PORT,
+      username: USERNAME,
+      password: PASSWORD,
+      database: DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
