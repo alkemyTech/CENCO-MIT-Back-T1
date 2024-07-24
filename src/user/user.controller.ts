e4b5dd1c33d29972a8476a78a9d5502abc959597
Genesis Sáez Soto <gnesis.vss@gmail.com>
2024-07-24 18:40:19 -0400

    feat: add user login with validations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UnauthorizedException } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post('signup')
   async create(@Body() createUserDto: CreateUserDto): Promise<{ message: string }> {
     await this.userService.create(createUserDto);
     return { message: 'User successfully created' };
+  }
+
+  @Post('login')
+  // From the body we get the parameters of the login defined in the dto file
+  async login(@Body() loginDto: LoginDto) {
+    try {
+      // returns the response from de service
+      return this.userService.login(loginDto)
+    } catch (error) {
+      // throw an unauthorized exception
+      throw new UnauthorizedException();
+    }
   }
 
   @Get()
