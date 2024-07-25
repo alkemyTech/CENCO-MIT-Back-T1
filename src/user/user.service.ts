f0f05988d0c5a71958e4d169b09458f5d6a1703b
Genesis Sáez Soto <gnesis.vss@gmail.com>
2024-07-25 10:49:03 -0400

    fix: Changed error handling in login to use BadRequestException
    Replaced HttpException with BadRequestException in the login method of UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -60,7 +60,7 @@ export class UserService {
 
     if (!userFoundByEmail) {
       // if the email doesn't exists, throw a bad request exception
-      throw new HttpException("The user doesn't exists", HttpStatus.BAD_REQUEST);
+      throw new BadRequestException("The user doesn't exists")
     }
 
     // Verify password
