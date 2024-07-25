d9ccee7e8cc11a2c6c545a6294033c22d74f6782
Genesis Sáez Soto <gnesis.vss@gmail.com>
2024-07-25 11:45:46 -0400

    fix: Changed the error message in role guard

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -4,25 +4,25 @@ import { Role } from "../user/entities/role.enum";
 import { ROLES_KEY } from "../decorators/has-roles.decorator";
 
 @Injectable()
-export class RolesGuard implements CanActivate{
-    constructor (private reflector: Reflector){}
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
 
-    canActivate(context: ExecutionContext): boolean {
-        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-            context.getHandler(),
-            context.getClass()
-        ]);
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]);
 
-        if (!requiredRoles) {
-            return true;
-          }
-          const { user } = context.switchToHttp().getRequest();
-          console.log(user);
-          const hasRequiredRole = requiredRoles.some((role) => user?.role?.includes(role));
-        
-          if(!hasRequiredRole){
-            throw new UnauthorizedException('You need an admin role to perform this action.');
-          }
-          return true;
+    if (!requiredRoles) {
+      return true;
     }
+    const { user } = context.switchToHttp().getRequest();
+    console.log(user);
+    const hasRequiredRole = requiredRoles.some((role) => user?.role?.includes(role));
+
+    if (!hasRequiredRole) {
+      throw new UnauthorizedException("You don't have the required permission to execute this action");
+    }
+    return true;
+  }
 }
