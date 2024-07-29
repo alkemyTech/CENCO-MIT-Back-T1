e2bebd14748d28132e46acda97f1869dba7c6819
Genesis Sáez Soto <gnesis.vss@gmail.com>
2024-07-29 15:49:29 -0400

    merge feat/update-user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
